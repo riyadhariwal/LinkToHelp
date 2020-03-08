594360bc08feaaadd7df160aea4f3cdb538704d9
M. Mishieru <mchan324@my.bcit.ca>
2020-03-08 06:37:47 -0700

    trying to merge/pull

---
+++ form/firebase.js
@@ -0,0 +1,23 @@
+<!-- The core Firebase JS SDK is always required and must be listed first -->
+<script src="https://www.gstatic.com/firebasejs/7.10.0/firebase-app.js"></script>
+
+    <!-- TODO: Add SDKs for Firebase products that you want to use
+https://firebase.google.com/docs/web/setup#available-libraries -->
+    <script src="https://www.gstatic.com/firebasejs/7.10.0/firebase-analytics.js"></script>
+
+    <script>
+// Your web app's Firebase configuration
+var firebaseConfig = {
+    apiKey: "AIzaSyBj5i7vGuqOTFnFe0xcqSnp0QMY9Ka--oU",
+    authDomain: "link2help-a3b51.firebaseapp.com",
+    databaseURL: "https://link2help-a3b51.firebaseio.com",
+    projectId: "link2help-a3b51",
+    storageBucket: "link2help-a3b51.appspot.com",
+    messagingSenderId: "576132785086",
+    appId: "1:576132785086:web:d0a1ed0c1aea16f4dd8fef",
+    measurementId: "G-ZFT3DFTGCV"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+firebase.analytics();
+</script>
